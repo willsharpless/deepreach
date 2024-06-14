7061000ceb8623ab2e6dbb426f6b2c13ff309c9e
willsharpless <williamsharpless.2@gmail.com>
2024-06-14 14:32:16 +0000

    Linear2D (dynamics), DeepReach2D (experiment)

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -182,6 +182,8 @@ class Experiment(ABC):
                             losses['diff_constraint_hom'].backward(retain_graph=True)
                             grads_PDE = []
                             for key, param in params.items():
+                                # print(key, param)
+                                # print(key, param.grad)
                                 grads_PDE.append(param.grad.view(-1))
                             grads_PDE = torch.cat(grads_PDE)
 
@@ -505,3 +507,58 @@ class Experiment(ABC):
 class DeepReach(Experiment):
     def init_special(self):
         pass
+
+class DeepReach2D(Experiment):
+    def init_special(self):
+        pass
+    
+    def validate(self, epoch, save_path, x_resolution, y_resolution, z_resolution, time_resolution):
+        was_training = self.model.training
+        self.model.eval()
+        self.model.requires_grad_(False)
+
+        plot_config = self.dataset.dynamics.plot_config()
+
+        state_test_range = self.dataset.dynamics.state_test_range()
+        x_min, x_max = state_test_range[plot_config['x_axis_idx']]
+        y_min, y_max = state_test_range[plot_config['y_axis_idx']]
+        # z_min, z_max = state_test_range[plot_config['z_axis_idx']]
+
+        times = torch.linspace(0, self.dataset.tMax, time_resolution)
+        xs = torch.linspace(x_min, x_max, x_resolution)
+        ys = torch.linspace(y_min, y_max, y_resolution)
+        # zs = torch.linspace(z_min, z_max, z_resolution)
+        xys = torch.cartesian_prod(xs, ys)
+        
+        fig = plt.figure(figsize=(5*len(times), 5*1))
+        for i in range(len(times)):
+            # for j in range(len(zs)):
+            j = 0
+            coords = torch.zeros(x_resolution*y_resolution, self.dataset.dynamics.state_dim + 1)
+            coords[:, 0] = times[i]
+            coords[:, 1:] = torch.tensor(plot_config['state_slices'])
+            coords[:, 1 + plot_config['x_axis_idx']] = xys[:, 0]
+            coords[:, 1 + plot_config['y_axis_idx']] = xys[:, 1]
+            # coords[:, 1 + plot_config['z_axis_idx']] = zs[j]
+
+            with torch.no_grad():
+                model_results = self.model({'coords': self.dataset.dynamics.coord_to_input(coords.cuda())})
+                values = self.dataset.dynamics.io_to_value(model_results['model_in'].detach(), model_results['model_out'].squeeze(dim=-1).detach())
+            
+            ax = fig.add_subplot(1, len(times), (j+1) + i)
+            ax.set_title('t = %0.2f' % (times[i])) #, plot_config['state_labels'][plot_config['z_axis_idx']], zs[j]))
+            s = ax.imshow(1*(values.detach().cpu().numpy().reshape(x_resolution, y_resolution).T <= 0), cmap='bwr', origin='lower', extent=(-1., 1., -1., 1.))
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            fig.colorbar(s, cax=cax) 
+        fig.savefig(save_path)
+        if self.use_wandb:
+            wandb.log({
+                'step': epoch,
+                'val_plot': wandb.Image(fig),
+            })
+        plt.close()
+
+        if was_training:
+            self.model.train()
+            self.model.requires_grad_(True)
