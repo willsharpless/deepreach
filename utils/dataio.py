~ fixed nl scaling ~ fixed weighting + capacity test + hopf decay opts + temporal weighting (log and loss) + MSE log + plot hopf soln

utils/dataio.py
@@ -11,7 +11,7 @@ from tqdm.autonotebook import tqdm
 class ReachabilityDataset(Dataset):
     def __init__(self, dynamics, numpoints, pretrain, pretrain_iters, tMin, tMax, counter_start, counter_end, num_src_samples, num_target_samples, 
                  use_hopf=False, hopf_pretrain=False, hopf_pretrain_iters=0, record_set_metrics=False,
-                 manual_load=False, load_packet=None, no_curriculum=False, use_bank=False, bank_name=None):
+                 manual_load=False, load_packet=None, no_curriculum=False, use_bank=False, bank_name=None, capacity_test=False):
         
         # print("Into the dataset!")
 
@@ -35,6 +35,7 @@ class ReachabilityDataset(Dataset):
         self.record_set_metrics = record_set_metrics
         self.no_curriculum = no_curriculum
         self.N = dynamics.N
+        self.capacity_test = capacity_test
 
         self.use_bank = use_bank
         if bank_name is None or bank_name == 'none': bank_name = "Bank_"+str(self.N)+"D_"+str(self.numpoints//10000)+"Mpts.npy"
@@ -68,13 +69,19 @@ class ReachabilityDataset(Dataset):
             elif self.N > 2:
 
                 LessLinear2D_interpolations = jl.load("LessLinear2D1i_interpolations_res1e-2_r15e-2.jld", "LessLinear2D_interpolations")
+
                 self.V_hopf_itp = LessLinear2D_interpolations["g0_m0_a0"]
+                if capacity_test:
+                    model_key = "g" + str(int(self.dynamics.gamma)) + "_m" + str(int(self.dynamics.mu)) + "_a"  + str(int(self.dynamics.alpha))
+                    self.V_hopf_itp = LessLinear2D_interpolations[model_key]
+
                 def V_N_hopf_itp(tXg):
                     V = 0 * tXg[0,:]
                     for i in range(self.N-1):
                         V += torch.from_numpy(self.fast_interp(self.V_hopf_itp, tXg[[0, 1, 2+i], :].numpy()).to_numpy())
                     return V
-                self.V_hopf = V_N_hopf_itp ## TODO: just use hopf solution (as in N==2 case)                
+                self.V_hopf = V_N_hopf_itp ## TODO: actually use hopf solution (as in N==2 case)
+
                 # fast_interp_exec = """
                 # function fast_interp_N(_V_itp, tXg)
                 #     Vg = zeros(size(tXg,2))
@@ -109,14 +116,23 @@ class ReachabilityDataset(Dataset):
                     # self.V_DP_itp = LessLinear2D_interpolations["g0_m0_a0"] #linear
                     # self.V_DP_itp = LessLinear2D_interpolations["g20_m0_a0"] #level 1
                     # self.V_DP_itp = LessLinear2D_interpolations["g20_m-20_a1"] #level 2
+                    
                     model_key = "g" + str(int(self.dynamics.gamma)) + "_m" + str(int(self.dynamics.mu)) + "_a"  + str(int(self.dynamics.alpha))
                     self.V_DP_itp = LessLinear2D_interpolations[model_key]
-                    def V_N_DP_itp(tXg):
+                    def V_N_DP_itp_combo(tXg):
                         V = 0 * tXg[0,:]
                         for i in range(self.N-1):
                             V += torch.from_numpy(self.fast_interp(self.V_DP_itp, tXg[[0, 1, 2+i], :].numpy()).to_numpy())
                         return V
-                    self.V_DP = V_N_DP_itp
+                    self.V_DP = V_N_DP_itp_combo
+
+                    self.V_DP_linear_itp = LessLinear2D_interpolations["g0_m0_a0"]
+                    def V_N_DP_linear_itp_combo(tXg):
+                        V = 0 * tXg[0,:]
+                        for i in range(self.N-1):
+                            V += torch.from_numpy(self.fast_interp(self.V_DP_linear_itp, tXg[[0, 1, 2+i], :].numpy()).to_numpy())
+                        return V
+                    self.V_DP_linear = V_N_DP_linear_itp_combo
 
             ## Define a fixed spatiotemporal grid to score Jaccard
 
@@ -175,6 +191,7 @@ class ReachabilityDataset(Dataset):
             ## Precompute value & safe-set on grid for ground truth
 
             self.values_DP_grid = self.V_DP(self.dynamics.input_to_coord(self.model_coords_grid_allt).t()).cuda()
+            self.values_DP_linear_grid = self.V_DP_linear(self.dynamics.input_to_coord(self.model_coords_grid_allt).t()).cuda()
             self.values_DP_grid_sub0_ixs = torch.argwhere(self.values_DP_grid <= 0).flatten().cuda()
 
             self.values_DP_grid_hi = self.V_DP(self.dynamics.input_to_coord(self.model_coords_grid_allt_hi).t()).cuda()

utils/losses.py
@@ -1,4 +1,7 @@
 import torch
+from math import exp, sqrt
+from scipy.special import erf
+from scipy.stats import truncnorm
 
 # uses real units
 def init_brt_hjivi_loss(dynamics, minWith, dirichlet_loss_divisor):
@@ -64,15 +67,15 @@ def init_brat_hjivi_loss(dynamics, minWith, dirichlet_loss_divisor):
     
     return brat_hjivi_loss
 
-def init_brt_hjivi_hopf_loss(dynamics, minWith, dirichlet_loss_divisor, hopf_loss_divisor, hopf_loss):
-
+def init_brt_hjivi_hopf_loss(experiment, minWith, dirichlet_loss_divisor, hopf_loss_divisor, hopf_loss, temporal_weighting):
+    
     ## Include a Linear Differencing Term
     if hopf_loss == 'lindiff':
 
-        def brt_hjivi_loss_hopf(state, value, dvdt, dvds, boundary_value, dirichlet_mask, output, hopf_value, learned_hopf_value, hopf_pretrain):
+        def brt_hjivi_loss_hopf(state, value, dvdt, dvds, boundary_value, dirichlet_mask, output, hopf_value, learned_hopf_value, epoch, state_time):
 
             dirichlet = value[dirichlet_mask] - boundary_value[dirichlet_mask]
-            if dynamics.deepreach_model == 'exact':
+            if experiment.dataset.dynamics.deepreach_model == 'exact':
                     dirichlet = output.squeeze(dim=-1)[dirichlet_mask]-0.0
 
             hopf_loss = learned_hopf_value - hopf_value
@@ -84,11 +87,11 @@ def init_brt_hjivi_hopf_loss(dynamics, minWith, dirichlet_loss_divisor, hopf_los
                 hopf_loss = torch.Tensor([0])
 
             # hopf pretraining
-            elif hopf_pretrain: 
+            elif experiment.dataset.hopf_pretrain: 
                 diff_constraint_hom = torch.Tensor([0])
 
             else:
-                ham = dynamics.hamiltonian(state, dvds)
+                ham = experiment.dataset.dynamics.hamiltonian(state, dvds)
                 # If we are computing BRT then take min with zero
                 if minWith == 'zero':
                     ham = torch.clamp(ham, max=0.0)
@@ -97,8 +100,28 @@ def init_brt_hjivi_hopf_loss(dynamics, minWith, dirichlet_loss_divisor, hopf_los
                 if minWith == 'target':
                     diff_constraint_hom = torch.max(diff_constraint_hom, value - boundary_value)
 
-                if dynamics.deepreach_model == 'exact':
+                if experiment.dataset.dynamics.deepreach_model == 'exact':
                     dirichlet = torch.Tensor([0]).cuda()
+
+                if temporal_weighting and epoch >= experiment.total_pretrain_iters:            
+                    ## moving-mean truncated normal params
+                    # mu = (epoch - experiment.dataset.total_pretrain_iters)/(experiment.epochs - experiment.dataset.total_pretrain_iters)
+                    # inv_sig = 1
+
+                    ## flattening truncated normal params
+                    a,b = 0,1
+                    mu, sigma_init, sigma_final, k = 0, 0.4, 5, 5
+                    e_perc, sigma = 0, sigma_init
+
+                    a_p, b_p = (a - mu) / sigma, (b - mu) / sigma
+                    weight = torch.tensor(truncnorm.pdf(state_time.cpu(), a_p, b_p, scale=sigma))
+                    diff_constraint_hom = torch.tensor(weight).cuda() * diff_constraint_hom
+
+            # if epoch >= experiment.total_pretrain_iters:
+            #     print("times   : ", state_time)
+            #     if temporal_weighting:
+            #         print("weight  :",  weight)
+            #     print("PDE loss:",  torch.abs(diff_constraint_hom).sum().item())
 
             return {'dirichlet': torch.abs(dirichlet).sum() / dirichlet_loss_divisor,
                     'hopf': torch.abs(hopf_loss).sum()  / hopf_loss_divisor,