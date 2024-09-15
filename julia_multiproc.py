6ede19ceaf2f6e3159689db9770dd87c4e6fad1e
willsharpless <williamsharpless.2@gmail.com>
2024-09-15 05:02:07 +0000

    bank_start working + clean, noticed load isnt balanced tho...

--- julia_multiproc.py
+++ julia_multiproc.py
@@ -22,6 +22,8 @@ class HopfJuliaPool(object):
 
     lock = None
     jl = None
+    interp_file_name = ""
+    write_output = True
     log_loc = "runs/test_run/julia_multiproc_logs" ## FIXME: actual run name, not 'test_run'
     master_log = "master_log.log"
     worker_id = 0
@@ -84,7 +86,6 @@ class HopfJuliaPool(object):
             while self.jobs:
                 for job in self.jobs:
                     if job.ready():
-                        # print(job.get())
                         pbar.update(1)
                         self.jobs.remove(job)
         print("Finished initializing solvers.")
@@ -93,8 +94,6 @@ class HopfJuliaPool(object):
     def init_worker(cls):
         
         cls.worker_id = os.getpid()
-        # cls.worker_log = f"worker_{worker_id}.log"
-        # sys.stdout = open(cls.worker_log, 'w')
         cls.worker_log = f"worker_{cls.worker_id}.log"
         cls.worker_log_full = os.path.join(cls.log_loc, cls.worker_log)
         print(f'Initializing worker {cls.worker_id}...')
@@ -105,21 +104,19 @@ class HopfJuliaPool(object):
         from juliacall import Main as jl, convert as jlconvert
         cls.jl = jl
 
-        exec_print = f"""
-
+        if cls.write_output:
+            exec_print = f"""\n
 # Set Output
 
 global log_f = open("{cls.worker_log_full}", "a")
 redirect_stdout(log_f)
-redirect_stderr(log_f)""" ## at least see partial Hopf_BRS print (until it presumably errors)
-        cls.jl.seval(exec_print)
+redirect_stderr(log_f)"""
+            cls.jl.seval(exec_print)
 
-        exec_load = f"""
+            print(f"\n\n ################ Worker {cls.worker_id} log ################\n\n")
 
+        exec_load = f"""\n
 # Load
-
-using Pkg
-Pkg.activate()
 
 using LinearAlgebra
 
@@ -128,20 +125,8 @@ using .HopfReachability: Hopf_BRS, Hopf_cd, make_grid, make_target, make_set_par
         cls.jl.seval(exec_load)
         print(exec_load)
 
-        ## Stdout Flusher, doesn't help/change juliacall muting
-        # exec_flush = """
-        # function log_flush()
-        #     flush(log_f)
-        #     close(log_f)
-        # end
-        # """
-        # cls.log_flush = cls.jl.seval(exec_flush)
-        # cls.flush_julia_procs = lambda: staticmethod(cls.log_flush()) # doesnt help
-
     @classmethod
     def init_solver(cls, dynamics_data, time_step, hopf_opt_p, settings):
-
-        # print('Setting up solver...')
         
         ## Store Class Attributes
         use_hopf, solve_grad, gt_metrics, hopf_warm_start = settings
@@ -155,10 +140,8 @@ using .HopfReachability: Hopf_BRS, Hopf_cd, make_grid, make_target, make_set_par
 
         ## Execute HopfReachability.jl's solve_BRS
         if cls.use_hopf:
-            hopf_setup_exec = f"""
-
+            hopf_setup_exec = f"""\n
 # Define the Problem
-
 
 ## System & Game
 A, B₁, B₂, = {dynamics_data["A"].numpy()}, {dynamics_data["B"].numpy()}, {dynamics_data["C"].numpy()}
@@ -189,27 +172,19 @@ P_in_f(gradVX) = reshape(hcat(gradVX[2:end]...), size(gradVX[1])..., length(grad
             print(hopf_setup_exec)
             cls.jl.seval(hopf_setup_exec)
 
-            solve_Hopf_BRS_exec = f"""
-
+            solve_Hopf_BRS_exec = f"""\n
 # Wrapper for Hopf Solver
 
 function solve_Hopf_BRS(X_in; P_in=nothing, return_grad=false)
-    println("About to solve hopf with")
-    println("VERSION", VERSION)
-    println(Sys.BINDIR)
-    println(Pkg.envdir())
-    println("\n X:")
-    print(Matrix(X_in))
-    println("\n")
-    println("return_grad=", return_grad)
+    println("Solving Hopf ... ")
     flush(log_f)
-    (XsT, VXsT), run_stats, opt_data, gradVXsT = Hopf_BRS(system, target, times; X=Matrix(X_in), th, input_shapes, game, opt_method=Hopf_cd, opt_p=opt_p_cd, P_in, warm=true, warm_pattern="temporal", printing=true)
-    println("solved hopf")
+    (XsT, VXsT), run_stats, opt_data, gradVXsT = Hopf_BRS(system, target, times; X=Matrix(X_in), th, input_shapes, game, opt_method=Hopf_cd, opt_p=opt_p_cd, P_in, warm=true, warm_pattern="temporal", printing=false)
+    println("Success!")
     flush(log_f)
     if return_grad
-        return VXsT[1], vcat(VXsT[2:end]...), P_in_f(gradVXsT)
+        return VXsT[1], vcat(VXsT[2:end]...), P_in_f(gradVXsT), run_stats[1]
     else
-        return VXsT[1], vcat(VXsT[2:end]...)
+        return VXsT[1], vcat(VXsT[2:end]...), run_stats[1]
     end
 end"""
             cls.solve_hopf_BRS = cls.jl.seval(solve_Hopf_BRS_exec)
@@ -222,21 +197,20 @@ end"""
 
         ## Interpolate the Dynamic Programming Solution
         if not cls.use_hopf or cls.gt_metrics:
-            print(f"Computing value with DP-Interpolations (not solving hopf), from LessLinear2D1i_interpolations_res1e-2_r{int(100 * dynamics_data['goalR_2d'])}e-2_c20.jld")
             cls.jl.seval("using JLD, JLD2, Interpolations")
 
             llnd_path = "value_fns/LessLinear/"
             # V_itp = cls.jl.load(llnd_path + "interps/old/lin2d_hopf_interp_linear.jld")["V_itp"]
-            V_DP_itp = cls.jl.load(llnd_path + f"interps/LessLinear2D1i_interpolations_res1e-2_r{int(100 * dynamics_data['goalR_2d'])}e-2_c20.jld", "LessLinear2D_interpolations")["g0_m0_a0"] # FIXME: flexible c param value
+            cls.interp_file_name = f"LessLinear2D1i_interpolations_res1e-2_r{int(100 * dynamics_data['goalR_2d'])}e-2_c20.jld"
+            V_DP_itp = cls.jl.load(llnd_path + f"interps/{cls.interp_file_name}", "LessLinear2D_interpolations")["g0_m0_a0"] # FIXME: flexible c param value
 
-            fast_interp_exec = """
-
+            fast_interp_exec = """\n
 # Warpper for Interpolation
-
 
 function fast_interp(_V_itp, tX_in; compute_grad=false)
     tX = Matrix(tX_in) # strange mp + torch + juliacall bug, only noticable here
-    println("tX (1+2d):", tX); flush(log_f)
+    # print("Interpolating Xi across t... ")
+    # flush(log_f)
     V = zeros(size(tX, 2))
     for i=1:length(V); V[i] = _V_itp(tX[:,i][end:-1:1]...); end # (assumes t in first row)
     J = zeros(size(tX, 2))
@@ -254,27 +228,27 @@ end"""
             def V_N_DP_linear_itp_combo(tXg):
                 J = 0 * tXg[0,:]
                 V = 0 * tXg[0,:]
+                print("Interpolating DP solution ... ")
                 for i in range(cls.N-1):
                     Ji, Vi = fast_interp(V_DP_itp, tXg[[0, 1, 2+i], :])
                     J, V = J+Ji, V+Vi
+                print("Success.")
                 return J, V
 
             def V_N_DP_itp_grad_combo(tXg):
                 J = 0 * tXg[0,:]
                 V = 0 * tXg[0,:]
                 DV = 0 * tXg[1:,:].T
+                print("Interpolating DP solution and gradients ... ")
                 for i in range(self.N-1):
                     Ji, Vi, DVi = fast_interp(V_DP_itp, tXg[[0, 1, 2+i], :], compute_grad=True)
                     J, V = J+Ji, V+Vi
                     DV[:, [0, 1+i]] += DVi # assumes xN first
+                print("Success.")
                 return J, V, DV
 
             cls.V_hopf_gt = V_N_DP_linear_itp_combo
             cls.V_hopf_gt_grad = V_N_DP_itp_grad_combo
-
-            # cls.V_hopf_gt = staticmethod(lambda tX: fast_interp(V_itp, tX))
-            # cls.V_hopf_gt_grad = staticmethod(lambda tX: fast_interp(V_itp, tX, compute_grad=True))
-            # print(cls.V_hopf_gt())
 
         return 
 
@@ -294,54 +268,33 @@ end"""
         if not cls.use_hopf or cls.gt_metrics:
             tXi = np.hstack([np.vstack(((j+1) * cls.ts * np.ones((1, Xi.shape[1])), Xi)) for j in range(cls.tp)])
 
-        ## Redirect Output to Log files
-        job_log = f"worker_{cls.worker_id}_job_{job_id}.log"
-        job_log_full = os.path.join(cls.log_loc, job_log)
-        # print(f"Worker {cls.worker_id}, job {job_id}: Solving Hopf formula... writing to {job_log}")
-        # sys.stdout = open(job_log_full, 'w')
-        # job_log_file = open(job_log_full, 'a')
-        # jl.seval(f"""
-        #     redirect_stdout(unsafe_string(pointer_from_objref(PyCall.pyimport('builtins').open))("{job_log_full}", "a"))
-        # """)
-        # sys.stdout.flush()
-        # redirect_stdout(open("{job_log_full}", "a"))
-        # redirect_stderr(open("{job_log_full}", "a"))
-
-        # cls.jl.seval(f"""
-
-        # global log_f = open("{job_log_full}", "a")
-        # redirect_stdout(log_f)
-        # redirect_stderr(log_f)
-
-        # """) ## cant see Hopf_BRS print this way?
-
-        # with contextlib.redirect_stdout(job_log_file), contextlib.redirect_stderr(job_log_file):
-
-        print(f"\n ########### Worker {cls.worker_id}, job {job_id} ###########\n")
-        
-        start_time = time.time()
+        print(f"\n\n ########### Worker {cls.worker_id}, job {job_id} ###########\n\n")
+        print(f"Solving {int(Xi.shape[1] * cls.tp)} points ({Xi.shape[1]} spatial, {cls.tp} time).")
 
         ## Compute Value (and Gradient) with Hopf, w/w/o Some Gradients for Warm-Starting Hopf
         if cls.use_hopf:
             if cls.solve_grad:
                 if tX_grad is not None:
-                    J, V, DxV = cls.V_hopf_grad_ws(Xi, tX_grad)
+                    print("\nWarm-starting the hopf solve with gradient data.")
+                    J, V, DxV, comp_time = cls.V_hopf_grad_ws(Xi, tX_grad)
                 else:
-                    J, V, DxV = cls.V_hopf_grad(Xi)
+                    J, V, DxV, comp_time = cls.V_hopf_grad(Xi)
             else:
                 if tX_grad is not None:
-                    J, V = cls.V_hopf_grad(Xi, tX_grad)
+                    print("\nWarm-starting the hopf solve with gradient data.")
+                    J, V, comp_time = cls.V_hopf_grad(Xi, tX_grad)
                 else:
-                    J, V = cls.V_hopf(Xi)
+                    J, V, comp_time = cls.V_hopf(Xi)
 
         ## Compute Value with Composed 2D Interpolation (for testing)
         else: 
+            print(f"\nComputing value with DP-Interpolations (not solving hopf), from {cls.interp_file_name}")
             if cls.solve_grad:
                 J, V, DxV = cls.V_hopf_gt_grad(tXi)
             else:
                 J, V = cls.V_hopf_gt(tXi)
 
-        mean_time = (time.time() - start_time) / split_size
+        mean_time = comp_time / split_size
 
         if cls.use_hopf:
             J = np.repeat(J, int(1/cls.ts))
@@ -351,7 +304,7 @@ end"""
 
             ## Solve Ground Truth
             if cls.gt_metrics:
-
+                print(f"\nComputing value with DP-Interpolations for ground truth, from {cls.interp_file_name}")
                 if cls.solve_grad:
                     J_gt, V_gt, DxV_gt = cls.V_hopf_gt_grad(tXi)
                 else:
@@ -363,6 +316,8 @@ end"""
                 if cls.solve_grad:
                     SE_grad = np.power((DxV_gt - DxV), 2).mean(dim=1) # mse per pt
                     MSE_grad = SE_grad.mean()
+        
+        print(f"\n Batch had accuracy of {MSE} and took {mean_time} s/pt")
 
         ## Store in Shared Memory
         with cls.lock:
@@ -384,19 +339,19 @@ end"""
                 if cls.solve_grad:
                     alg_data[job_id, 4] = MSE_grad
             
-            # ## Write Job Log to Master Log
-            # with open(job_log_full, 'r') as wlog, open(os.path.join(cls.log_loc, cls.master_log), 'a') as mlog:
-            #     shutil.copyfileobj(wlog, mlog)
+            ## Write Job Log to Master Log
+            with open(cls.worker_log_full, 'r') as wlog, open(os.path.join(cls.log_loc, cls.master_log), 'a') as mlog:
+                shutil.copyfileobj(wlog, mlog)
         
-        ## Delete job log and restore Julia output
+        # Delete job log and restore Julia output
         # try:
-        #     os.remove(job_log_full)
+        #     os.remove(cls.worker_log_full)
         # except OSError as e:
-        #     print(f"Error deleting worker log {job_log}: {e}")
+        #     print(f"Error deleting worker log {cls.worker_log_full}: {e}")
         # jl.seval(f"""
         #     redirect_stdout(stdout)
         # """)
-        # job_log_file.close()
+        # cls.worker_log_full.close()
 
         return (job_id, mean_time)
 
@@ -446,43 +401,17 @@ end"""
                 Xi = bank[i*split_size: i*split_size + split_spatial_pts, 1:self.N+1].T
                 job = self.pool.apply_async(self.solve_hopf, (Xi, i*split_size, shm_data))
                 self.jobs.append(job)
-                if i == 0: print(Xi.shape); print(type(Xi))
-                # print("jobs len:", len(self.jobs))
 
             # for job in self.jobs:
             #     job_id, mean_time = job.get()
             #     # TODO: in invst, will store alg_data[job_id, 0] = self.alg_iter (automatically)
             #     pbar.update(1)
             
-            print("While jobs, jobs len:", len(self.jobs))
             while self.jobs:
                 for job in self.jobs:
                     if job.ready():
-                        # print("Job ready!")
-                        print(job.get())
                         pbar.update(1)
                         self.jobs.remove(job)
-            
-            # while self.jobs:
-            #     for job in self.jobs:
-            #         try:
-            #             if job.ready():
-            #                 # print("Job ready!")
-            #                 pbar.update(1)
-            #                 self.jobs.remove(job)
-            #         except Exception as e:
-            #             print(f"JOB OBJECT BROKEN: {e}")
-            #             self.jobs.remove(job)
-
-        # ## Flush In Case of Error (doesnt help/change output)
-        # for i in range(self.num_hopf_workers):
-        #     job = self.pool.apply_async(self.flush_julia_procs)
-        #     self.jobs.append(job)
-
-        # while self.jobs:
-        #     for job in self.jobs:
-        #         if job.ready():
-        #             self.jobs.remove(job)
         
         print("Finished solving bank starter.")
 
@@ -513,7 +442,6 @@ end"""
         pass
 
 from dynamics.dynamics import LessLinearND
-# import logging
 
 if __name__ == '__main__':
     mp.set_start_method('spawn', force=True)
@@ -528,32 +456,16 @@ if __name__ == '__main__':
     hopf_opt_p = {"vh":0.01, "stepsz":1, "tol":1e-3, "decay_stepsz":100, "conv_runs_rqd":1, "max_runs":1, "max_its":100} 
 
     hjpool = HopfJuliaPool(dynamics_data, time_step, hopf_opt_p,
-                            use_hopf=True, solve_grad=False, hopf_warm_start=False, gt_metrics=True, num_hopf_workers=1)
+                            use_hopf=True, solve_grad=False, hopf_warm_start=False, gt_metrics=True, num_hopf_workers=4)
 
     # bank_params = {"bank_total":200000, "bank_start":100000, "bank_invst":10000}
-    bank_params = {"bank_total":200, "bank_start":10, "bank_invst":10}
-    shm_data = hjpool.solve_bank_start(bank_params, n_splits=1)
+    bank_params = {"bank_total":200, "bank_start":100, "bank_invst":10}
+    shm_data = hjpool.solve_bank_start(bank_params, n_splits=10)
     
     shm_states_id, shm_states_shape, shm_algdat_id, shm_algdat_shape = shm_data
     shm_states, shm_algdat = SharedMemory(name=shm_states_id), SharedMemory(name=shm_algdat_id)
     bank = np.ndarray(shm_states_shape, dtype=np.float32, buffer=shm_states.buf)
     alg_data = np.ndarray(shm_algdat_shape, dtype=np.float32, buffer=shm_algdat.buf)
-
-    print()
-
-    # ## debugging serialization error...
-    # hjpool.init_worker()
-    # settings = (True, False, True, False)
-    # hjpool.init_solver(dynamics_data, time_step, hopf_opt_p, settings)
-
-    # X = np.random.uniform(-1, 1, (10, hjpool.N))
-    # # result = hjpool.V_hopf(X)
-
-    # tX = np.hstack(0.1 * np.ones(10,1), X)
-    # result = hjpool.V_hopf_gt(X)
-
-    # import pickle
-    # pickle.dumps(result)
 
     print("He hecho\n")
             
