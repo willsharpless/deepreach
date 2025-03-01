bb3c34020cbcaa32aaa3cea27199810728192497
willsharpless <williamsharpless.2@gmail.com>
2025-03-01 02:37:15 +0000

    retested and ran benchmark problem

---
+++ exec_scripts/run_ZLLS_benchmark.sh
@@ -0,0 +1,78 @@
+
+
+## Init
+export CUDA_VISIBLE_DEVICES=1
+
+readonly wandb_project="deepreach_hopf_benchmarks"
+# readonly experiment_name="bench_holder" ## must overwrite to save data, will save model_final?
+
+readonly summary_dir_name="./summaries/ZLLS_hld100_hgld250_50D_300k"
+readonly experiments_dir="./runs"
+readonly dim="50"
+readonly goalR="0.25"
+readonly num_epochs="300000"
+readonly bs="65000"
+readonly lr="1e-6"
+
+readonly base_args="run_experiment.py --use_wandb --experiments_dir $experiments_dir --wandb_project $wandb_project --N $dim --goalR $goalR --num_epochs $num_epochs --numpoints $bs --lr_std $lr"
+
+readonly bench_mag="20"
+# readonly bench1="--gamma $bench_mag --mu 0 --alpha 0"
+readonly bench2="--gamma -$bench_mag --mu 0 --alpha 0"
+readonly bench3="--gamma -$bench_mag --mu -$bench_mag --alpha 1"
+readonly bench4="--gamma $bench_mag --mu -$bench_mag --alpha 1"
+
+## Method
+
+# readonly supervisor="./runs/LL50D_DR_linear_r25"
+readonly supervisor="./linear_supervisors/LL50D_DR_linear_r25_light"
+
+# readonly method_args="--dynamics_class LessLinearND --baseline" ## Baseline
+# readonly method_args="--dynamics_class LessLinearND --hopf_loss_decay --hopf_loss_decay_type linear --hopf_loss_decay_w 0.8 --diff_con_loss_incr --load_hopf_model --load_hopf_model_name $supervisor" ## Decayed LSS
+# readonly method_args="--dynamics_class LessLinearND --nl_scale --nl_scale_epoch_step 2000 --nl_scale_epoch_post 50000  --load_hopf_model --load_hopf_model_name $supervisor" ## NL Curriculum Scale LSS
+
+# readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 100 --hopf_grad_loss_divisor 250"
+# readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 1000 --hopf_grad_loss_divisor 4000"
+# readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 500 --hopf_grad_loss_divisor 4000"
+
+FIXME: #add self adjusted flags here, I named all the subsequent experiments "AUTOTUNE"
+readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 500 --hopf_grad_loss_divisor 1000"
+
+# readonly method_args="--capacity" ## Capacity
+
+## Execute
+
+mkdir -p $summary_dir_name
+
+name="ZLLS_hld1k_hgld4k_50D_300k_b2"
+experiment_name="--experiment_name $name"
+wandb_name="--wandb_name ZLLS_hld1k_hgld4k_50D_300k_b2"
+python $base_args $bench2 $method_args $experiment_name $wandb_name
+# echo $base_args $bench2 $method_args $experiment_name $wandb_name
+
+cp wandb/latest-run/files/wandb-summary.json $summary_dir_name/wandb-summary_b2.json
+cp $experiments_dir/$name/training/checkpoints/model_final.pth $summary_dir_name/model_final_b2.pth
+cp $experiments_dir/$name/training/checkpoints/BRS_validation_plot.png $summary_dir_name/BRS_validation_plot_b2.png
+
+name="ZLLS_AUTOTUNE_50D_300k_b3"
+experiment_name="--experiment_name $name"
+wandb_name="--wandb_name ZLLS_AUTOTUNE_50D_300k_b3"
+python $base_args $bench3 $method_args $experiment_name $wandb_name
+# echo $base_args $bench3 $method_args $experiment_name $wandb_name
+
+cp wandb/latest-run/files/wandb-summary.json $summary_dir_name/wandb-summary_b3.json
+cp $experiments_dir/$name/training/checkpoints/model_final.pth $summary_dir_name/model_final_b3.pth
+cp $experiments_dir/$name/training/checkpoints/BRS_validation_plot.png $summary_dir_name/BRS_validation_plot_b3.png
+
+name="ZLLS_AUTOTUNE_50D_300k_b4"
+experiment_name="--experiment_name $name"
+wandb_name="--wandb_name ZLLS_AUTOTUNE_50D_300k_b4"
+python $base_args $bench4 $method_args $experiment_name $wandb_name
+# echo $base_args $bench4 $method_args $experiment_name $wandb_name
+
+cp wandb/latest-run/files/wandb-summary.json $summary_dir_name/wandb-summary_b4.json
+cp $experiments_dir/$name/training/checkpoints/model_final.pth $summary_dir_name/model_final_b4.pth
+cp $experiments_dir/$name/training/checkpoints/BRS_validation_plot.png $summary_dir_name/BRS_validation_plot_b4.png
+
+cd $summary_dir_name
+jq -s '.' wandb-summary_b2.json wandb-summary_b3.json wandb-summary_b4.json > benchmark_tally.json
--- exec_scripts/run_benchmark.sh
+++ exec_scripts/run_benchmark.sh
@@ -1,78 +1,42 @@
 
+###### Linear Supervision for Nonlinear, High-Dimensional Neural Control and Differential Games
+###### Sharpless (wsharpless@ucsd.edu), Feng, Bansal, Herbert - 2024, L4DC 
+###### Benchmark Script
 
 ## Init
-export CUDA_VISIBLE_DEVICES=1
 
-readonly wandb_project="deepreach_hopf_benchmarks"
-# readonly experiment_name="bench_holder" ## must overwrite to save data, will save model_final?
+readonly base_args="--dynamics_class LessLinearND --N 10 --goalR 0.25 --solve_grad"
+readonly lin_sys_args="--gamma 0 --mu 0 --alpha 0"
+readonly nlin_sys_args="--gamma 20 --mu 0 --alpha 0"
 
-readonly summary_dir_name="./summaries/ZLLS_hld100_hgld250_50D_300k"
-readonly experiments_dir="./runs"
-readonly dim="50"
-readonly goalR="0.25"
-readonly num_epochs="300000"
-readonly bs="65000"
-readonly lr="1e-6"
+readonly use_wandb="--use_wandb" # "" if you dont want wandb
+readonly wandb_project="deepreach_hopf_retest"
 
-readonly base_args="run_experiment.py --use_wandb --experiments_dir $experiments_dir --wandb_project $wandb_project --N $dim --goalR $goalR --num_epochs $num_epochs --numpoints $bs --lr_std $lr"
+## Make Linear Model for Semi-Supervision (hopf or vanilla deepreach)
 
-readonly bench_mag="20"
-# readonly bench1="--gamma $bench_mag --mu 0 --alpha 0"
-readonly bench2="--gamma -$bench_mag --mu 0 --alpha 0"
-readonly bench3="--gamma -$bench_mag --mu -$bench_mag --alpha 1"
-readonly bench4="--gamma $bench_mag --mu -$bench_mag --alpha 1"
+# via DeepReach
+python run_experiment.py $base_args $lin_sys_args --num_epochs 30000 --baseline --experiment_name L10D_DR_linear --experiment_name L10D_DR $use_wandb --wandb_project $wandb_project --wandb_name L10D_DR --counter_end 20000
 
-## Method
+# from Hopf values (requires julia)
+python run_experiment.py $base_args $lin_sys_args --num_epochs 30000 --hopf_loss lin_val_diff --solve_hopf --hopf_loss_decay --hopf_loss_decay_type linear --hopf_loss_decay_w 20 --experiment_name L10D_hopf_linear_dynamic $use_wandb --wandb_project $wandb_project --wandb_name L10D_hopf_dyanmic
 
-# readonly supervisor="./runs/LL50D_DR_linear_r25"
-readonly supervisor="./linear_supervisors/LL50D_DR_linear_r25_light"
+# from bank of Hopf values
+python run_experiment.py $base_args $lin_sys_args --just_make_hopf_bank --hopf_loss lin_val_diff --solve_hopf # make bank
+python run_experiment.py $base_args $lin_sys_args --num_epochs 30000 --hopf_loss lin_val_diff --use_bank --bank_name Bank_Hopf_mp_10D_2Mpts_r25e-2_g0m0a0.npy --hopf_loss_decay --hopf_loss_decay_type linear --hopf_loss_decay_w 20 --experiment_name L10D_hopfb_linear_bank $use_wandb --wandb_project $wandb_project --wandb_name L10D_hopf_bank
 
-# readonly method_args="--dynamics_class LessLinearND --baseline" ## Baseline
-# readonly method_args="--dynamics_class LessLinearND --hopf_loss_decay --hopf_loss_decay_type linear --hopf_loss_decay_w 0.8 --diff_con_loss_incr --load_hopf_model --load_hopf_model_name $supervisor" ## Decayed LSS
-# readonly method_args="--dynamics_class LessLinearND --nl_scale --nl_scale_epoch_step 2000 --nl_scale_epoch_post 50000  --load_hopf_model --load_hopf_model_name $supervisor" ## NL Curriculum Scale LSS
+## Make Nonlinear Model 
 
-# readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 100 --hopf_grad_loss_divisor 250"
-# readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 1000 --hopf_grad_loss_divisor 4000"
-# readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 500 --hopf_grad_loss_divisor 4000"
+readonly supervisor="--load_hopf_model --load_hopf_model_name ./runs/L10D_hopfb_linear_bank" # can use any of the above for supervisor
+readonly hopf_args="--hopf_loss lin_val_grad_diff $supervisor"
 
-FIXME: #add self adjusted flags here, I named all the subsequent experiments "AUTOTUNE"
-readonly method_args="--dynamics_class LessLinearNDlambda --hopf_loss lin_val_grad_diff --load_hopf_model --load_hopf_model_name $supervisor --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 500 --hopf_grad_loss_divisor 1000"
+# baseline (vanilla DeepReach)
+python run_experiment.py $base_args $nlin_sys_args --baseline --num_epochs 100000 --experiment_name LL10D_DR $use_wandb --wandb_project $wandb_project --wandb_name LL10D_DR
 
-# readonly method_args="--capacity" ## Capacity
+# Linear Supervision w/ Decay
+python run_experiment.py $base_args $nlin_sys_args $hopf_args --hopf_loss_decay --hopf_loss_decay_type linear --hopf_loss_decay_w 0.75 --diff_con_loss_incr --num_epochs 10000 --experiment_name LL10D_LS_decay $use_wandb --wandb_project $wandb_project --wandb_name LL10D_LS_decay
 
-## Execute
+# Linear Supervision w/ Augmentation
+python run_experiment.py --dynamics_class LessLinearNDlambda --N 10 --goalR $goalR $nlin_sys_args $hopf_args --zerolambda_LS --LS_w_time_curr --hopf_pretrain_iters 1 --hopf_loss_divisor 50 --hopf_grad_loss_divisor 250 --num_epochs 100000 --experiment_name LL10D_LS_aug $use_wandb --wandb_project $wandb_project --wandb_name LL10D_LS_aug
 
-mkdir -p $summary_dir_name
-
-name="ZLLS_hld1k_hgld4k_50D_300k_b2"
-experiment_name="--experiment_name $name"
-wandb_name="--wandb_name ZLLS_hld1k_hgld4k_50D_300k_b2"
-python $base_args $bench2 $method_args $experiment_name $wandb_name
-# echo $base_args $bench2 $method_args $experiment_name $wandb_name
-
-cp wandb/latest-run/files/wandb-summary.json $summary_dir_name/wandb-summary_b2.json
-cp $experiments_dir/$name/training/checkpoints/model_final.pth $summary_dir_name/model_final_b2.pth
-cp $experiments_dir/$name/training/checkpoints/BRS_validation_plot.png $summary_dir_name/BRS_validation_plot_b2.png
-
-name="ZLLS_AUTOTUNE_50D_300k_b3"
-experiment_name="--experiment_name $name"
-wandb_name="--wandb_name ZLLS_AUTOTUNE_50D_300k_b3"
-python $base_args $bench3 $method_args $experiment_name $wandb_name
-# echo $base_args $bench3 $method_args $experiment_name $wandb_name
-
-cp wandb/latest-run/files/wandb-summary.json $summary_dir_name/wandb-summary_b3.json
-cp $experiments_dir/$name/training/checkpoints/model_final.pth $summary_dir_name/model_final_b3.pth
-cp $experiments_dir/$name/training/checkpoints/BRS_validation_plot.png $summary_dir_name/BRS_validation_plot_b3.png
-
-name="ZLLS_AUTOTUNE_50D_300k_b4"
-experiment_name="--experiment_name $name"
-wandb_name="--wandb_name ZLLS_AUTOTUNE_50D_300k_b4"
-python $base_args $bench4 $method_args $experiment_name $wandb_name
-# echo $base_args $bench4 $method_args $experiment_name $wandb_name
-
-cp wandb/latest-run/files/wandb-summary.json $summary_dir_name/wandb-summary_b4.json
-cp $experiments_dir/$name/training/checkpoints/model_final.pth $summary_dir_name/model_final_b4.pth
-cp $experiments_dir/$name/training/checkpoints/BRS_validation_plot.png $summary_dir_name/BRS_validation_plot_b4.png
-
-cd $summary_dir_name
-jq -s '.' wandb-summary_b2.json wandb-summary_b3.json wandb-summary_b4.json > benchmark_tally.json
+# capacity test (supervision with ground truth, SLOW)
+python run_experiment.py $base_args $nlin_sys_args --capacity_test --num_epochs 100000 --experiment_name LL10D_capacity $use_wandb --wandb_project $wandb_project --wandb_name LL10D_capacity
