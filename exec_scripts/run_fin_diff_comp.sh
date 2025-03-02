646a91f32f5aac7b440407ffff24e5de224c160f
willsharpless <williamsharpless.2@gmail.com>
2025-03-02 23:47:34 +0000

    Eoh

--- exec_scripts/run_fin_diff_comp.sh
+++ exec_scripts/run_fin_diff_comp.sh
@@ -27,29 +27,29 @@ fd_dxs="--fd_dxs 0.7 0.5 0.3 0.1"
 fd_dts="--fd_dts 0.05 0.03 0.02 0.01"
 
 for i in $(seq 1 $seeds); do
-    echo run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 3000 --numpoints 1000 --lr_std 1e-2 $fd_names --seed $i
+    python run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 3000 --numpoints 1000 --lr_std 1e-2 $fd_names --seed $i
 
-    echo run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 3000 --numpoints 1000 --lr_std 1e-3 $fd_names --seed $i
+    python run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 3000 --numpoints 1000 --lr_std 1e-3 $fd_names --seed $i
 
-    echo run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 1000 --lr_std 1e-5 $fd_names --seed $i
+    python run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 1000 --lr_std 1e-5 $fd_names --seed $i
 
-    echo run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 1000 --lr_std 1e-6 $fd_names --seed $i
+    python run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 1000 --lr_std 1e-6 $fd_names --seed $i
 
-    echo run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 10000 --lr_std 1e-5 $fd_names --seed $i
+    python run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 10000 --lr_std 1e-5 $fd_names --seed $i
 
-    echo run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 10000 --lr_std 1e-6 $fd_names --seed $i
+    python run_experiment.py $base_args $lin_sys_args --fin_diff $fd_as $fd_dxs $fd_dts --num_epochs 30000 --numpoints 10000 --lr_std 1e-6 $fd_names --seed $i
 done
 
 # DeepReach/PINN baseline
 
 for i in $(seq 1 $seeds); do
-    echo run_experiment.py $base_args $lin_sys_args --num_epochs 30000 --numpoints 65000 --lr_std 1e-5 --baseline --counter_end 20000 $dr_names --seed $i
+    python run_experiment.py $base_args $lin_sys_args --num_epochs 30000 --numpoints 65000 --lr_std 1e-5 --baseline --counter_end 20000 $dr_names --seed $i
 done
 
 # ## Solve Nonlinear Model 
 
 # # DeepReach/PINN baseline
-# echo run_experiment.py $base_args $nlin_sys_args --baseline --num_epochs 100000 --experiment_name LL${N}D_DR $use_wandb --wandb_project $wandb_project --wandb_name LL${N}D_DR
+# python run_experiment.py $base_args $nlin_sys_args --baseline --num_epochs 100000 --experiment_name LL${N}D_DR $use_wandb --wandb_project $wandb_project --wandb_name LL${N}D_DR
 
 # # Finite Differencing
-# echo run_experiment.py $base_args $nlin_sys_args --num_epochs 100000 --experiment_name LL${N}D_FD $use_wandb --wandb_project $wandb_project --wandb_name LL${N}D_FD
+# python run_experiment.py $base_args $nlin_sys_args --num_epochs 100000 --experiment_name LL${N}D_FD $use_wandb --wandb_project $wandb_project --wandb_name LL${N}D_FD
