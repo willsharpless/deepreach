fxd model loading bug, mulob loss still has a flipped sign or smth, grad super untested

experiments/experiments.py
@@ -38,10 +38,10 @@ class Experiment(ABC):
     def _load_checkpoint(self, epoch):
         if epoch == -1:
             model_path = os.path.join(self.experiment_dir, 'training', 'checkpoints', 'model_final.pth')
-            self.model.load_state_dict(torch.load(model_path)['model'])
+            self.model.load_state_dict(torch.load(model_path, weights_only=True)['model'])
         else:
             model_path = os.path.join(self.experiment_dir, 'training', 'checkpoints', 'model_epoch_%04d.pth' % epoch)
-            self.model.load_state_dict(torch.load(model_path)['model'])
+            self.model.load_state_dict(torch.load(model_path, weights_only=True)['model'])
 
     def validate(self, epoch, save_path, x_resolution, y_resolution, z_resolution, time_resolution):
         was_training = self.model.training
@@ -315,22 +315,34 @@ class Experiment(ABC):
                         if self.dataset.load_decomposed_models:
                             # note, decomposed values inferred on data wo lambda, and appropriately shifted if augmented for pde loss (but not ss losses)
                             
-                            with torch.inference_mode():
-                                if not self.dataset.lambda_var:
-                                    loaded_model_results_1 = self.dataset.loaded_model_1({'coords': model_input['model_coords']})
-                                    loaded_model_results_2 = self.dataset.loaded_model_2({'coords': model_input['model_coords']})
-                                else: 
-                                    loaded_model_results_1 = self.dataset.loaded_model_1({'coords': model_input['model_coords'][..., :-1]})
-                                    loaded_model_results_2 = self.dataset.loaded_model_2({'coords': model_input['model_coords'][..., :-1]})
+                            if not self.dataset.solve_grad:
+                                decomposed_grads_1, decomposed_grads_2 = None, None
+                                
+                                with torch.inference_mode():
+                                    load_coords = model_input['model_coords'] if not self.dataset.lambda_var else model_input['model_coords'][..., :-1]
+
+                                    loaded_model_results_1 = self.dataset.loaded_model_1({'coords': load_coords})
+                                    loaded_model_results_2 = self.dataset.loaded_model_2({'coords': load_coords})
+
+                                    decomposed_values_1 = self.dataset.loaded_dynamics_1.io_to_value(loaded_model_results_1['model_in'], loaded_model_results_1['model_out'].squeeze(dim=-1)).detach()
+                                    decomposed_values_2 = self.dataset.loaded_dynamics_2.io_to_value(loaded_model_results_2['model_in'], loaded_model_results_2['model_out'].squeeze(dim=-1)).detach()
+                            
+                            else:
+                                load_coords = model_input['model_coords'] if not self.dataset.lambda_var else model_input['model_coords'][..., :-1]
+
+                                loaded_model_results_1 = self.dataset.loaded_model_1({'coords': load_coords})
+                                loaded_model_results_2 = self.dataset.loaded_model_2({'coords': load_coords})
 
                                 decomposed_values_1 = self.dataset.loaded_dynamics_1.io_to_value(loaded_model_results_1['model_in'], loaded_model_results_1['model_out'].squeeze(dim=-1)).detach()
                                 decomposed_values_2 = self.dataset.loaded_dynamics_2.io_to_value(loaded_model_results_2['model_in'], loaded_model_results_2['model_out'].squeeze(dim=-1)).detach()
+                        
+                                decomposed_grads_1 = self.dataset.loaded_dynamics_1.io_to_dv(loaded_model_results_1['model_in'], loaded_model_results_1['model_out'].squeeze(dim=-1)).detach() # NOTE: keeping time grad too now, add [..., 1:] otherwise
+                                decomposed_grads_2 = self.dataset.loaded_dynamics_2.io_to_dv(loaded_model_results_2['model_in'], loaded_model_results_2['model_out'].squeeze(dim=-1)).detach() 
 
+                            if hasattr(self.dataset.loaded_dynamics_2, 'avoid_only') and self.dataset.loaded_dynamics_2.avoid_only:
+                                decomposed_values_2 = -1 * decomposed_values_2 # avoid_only defined positively in DR, so flip
                                 if self.dataset.solve_grad:
-                                    decomposed_grads_1 = self.dataset.loaded_dynamics_1.io_to_dv(loaded_model_results_1['model_in'], loaded_model_results_1['model_out'].squeeze(dim=-1)).detach() # NOTE: keeping time grad too now, add [..., 1:] otherwise
-                                    decomposed_grads_2 = self.dataset.loaded_dynamics_2.io_to_dv(loaded_model_results_2['model_in'], loaded_model_results_2['model_out'].squeeze(dim=-1)).detach()
-                                else:
-                                    decomposed_grads_1, decomposed_values_2 = None, None
+                                    decomposed_grads_2[..., 1:] = -1 * decomposed_grads_2[..., 1:]
 
                         # elif self.dataset.use_gt_decomposed:
                         else:
@@ -341,7 +353,7 @@ class Experiment(ABC):
                             decomposed_values_1, decomposed_values_2 = gt['gt_decomposed_values_1'], gt['gt_decomposed_values_2']
 
                             if self.dataset.solve_grad:
-                                decomposed_grads_1, decomposed_grads_2 =  gt['gt_decomposed_grads_1'], gt['gt_decomposed_grads_2']
+                                decomposed_grads_1, decomposed_grads_2 = gt['gt_decomposed_grads_1'], gt['gt_decomposed_grads_2']
                             else:
                                 decomposed_grads_1, decomposed_grads_2 = None, None
 
@@ -623,8 +635,8 @@ class Experiment(ABC):
                     np.savetxt(os.path.join(checkpoints_dir, 'train_losses_epoch_%04d.txt' % (epoch+1)),
                         np.array(train_losses))
                     self.validate(
-                        # epoch=epoch+1, save_path=os.path.join(checkpoints_dir, 'BRS_validation_plot_epoch_%04d.png' % (epoch+1)),
-                        epoch=epoch+1, save_path=os.path.join(checkpoints_dir, 'BRS_validation_plot.png'), # overwriting to save data
+                        epoch=epoch+1, save_path=os.path.join(checkpoints_dir, 'BRS_validation_plot_epoch_%04d.png' % (epoch+1)),
+                        # epoch=epoch+1, save_path=os.path.join(checkpoints_dir, 'BRS_validation_plot.png'), # overwriting to save data
                         x_resolution = val_x_resolution, y_resolution = val_y_resolution, z_resolution=val_z_resolution, time_resolution=val_time_resolution)
                 if self.timing: print("Checkpointing took:", time.time() - start_time_2)
 
@@ -869,7 +881,7 @@ class DeepReachHopf(Experiment):
         solve_times = torch.linspace(0, self.dataset.tMax, time_resolution)
         if self.dataset.dynamics.name == "Conveyor":
             if self.dataset.dynamics.avoid_type == "ball":
-                lambda_vals = torch.tensor([1., 0.1, 0., -0.2, -1.])
+                lambda_vals = torch.tensor([1., 0.1, 0., -0.3, -1.])
             else:
                 lambda_vals = torch.tensor([1., 0.05, 0., -0.2, -1.])
         else:
@@ -891,9 +903,9 @@ class DeepReachHopf(Experiment):
                 states_input = self.dataset.model_states_grid
                 times = torch.full((self.dataset.n_grid_pts_2d, 1), solve_times[i]).cuda()
                 input = torch.cat((times, states_input), dim=1) 
+                input[..., -1] = lambda_vals[j]/self.dataset.dynamics.state_var[-1] + 0*input[..., -1] # certain lambda
                 
                 coords = self.dataset.dynamics.input_to_coord(input)
-                coords[..., -1] = lambda_vals[j] + 0*coords[..., -1] # certain lambda
                 states = coords[:, 1:]
                 n_grid_len = self.dataset.X1g.size()[0]
                 
@@ -919,9 +931,17 @@ class DeepReachHopf(Experiment):
                     xlims, ylims = (-1.25, 1.25), (-0.5, 2.)
 
                 with torch.no_grad():
-                    model_results = self.model({'coords': input.cuda()})
+                    model_results = self.model({'coords': input.clone().cuda()}).copy()
                     values_learned = self.dataset.dynamics.io_to_value(model_results['model_in'].detach(), model_results['model_out'].squeeze(dim=-1).detach()).reshape(n_grid_len, n_grid_len).cpu()
-                
+                    
+                    if testing:
+                        if j == 0:
+                            model_results_1 = self.dataset.loaded_model_1({'coords': input[..., :-1].clone().cuda()}).copy()
+                            values_learned = self.dataset.loaded_dynamics_1.io_to_value(model_results_1['model_in'].detach(), model_results_1['model_out'].squeeze(dim=-1).detach()).detach().reshape(n_grid_len, n_grid_len).cpu()
+                        elif j == 4:
+                            model_results_2 = self.dataset.loaded_model_2({'coords': input[..., :-1].cuda()}).copy()
+                            values_learned = self.dataset.loaded_dynamics_2.io_to_value(model_results_2['model_in'].detach(), model_results_2['model_out'].squeeze(dim=-1).detach()).detach().reshape(n_grid_len, n_grid_len).cpu()
+                    
                 ax = fig.add_subplot(len(lambda_vals), len(solve_times), j*len(lambda_vals) + i + 1)
                 ax.set_title(f"t = {solve_times[i]:0.1f}, lambda = {lambda_vals[j]:1.1f}")
                 
@@ -932,7 +952,7 @@ class DeepReachHopf(Experiment):
                         plot_values,
                         levels=levels, extend="both", cmap=RdWhBl_vscaled)
                 
-                if values_gt.min() < 0. < values_gt.max() and (abs(lambda_vals[j]) in [0., 1.]):
+                if (abs(lambda_vals[j]) in [0., 1.]) and values_gt.min() < 0. < values_gt.max():
                     ax.contour(self.dataset.X1g * self.dataset.dynamics.state_var[0] + self.dataset.dynamics.state_mean[0], 
                                 self.dataset.X2g * self.dataset.dynamics.state_var[1] + self.dataset.dynamics.state_mean[1],
                                 values_gt, 

run_experiment.py
@@ -47,7 +47,6 @@ if __name__ == '__main__':
     p.add_argument('--reset_loss_w', action='store_true', default=False, required=False, help='Resets the loss weights to their values at the beginning of training (pre-decay)')
     p.add_argument('--reset_loss_period', type=int, default=500, required=False, help='The loss weight reset period')
     p.add_argument('--zerolambda_LS', action='store_true', default=False, required=False, help='Will limit linear supervision to loss on lambda = 0 data (for lambda-variation models only AND loaded models)')
-    p.add_argument('--LS_w_time_curr', action='store_true', default=False, required=False, help='Do linear supervision with a temporal curriculum')
 
     p.add_argument('--gt_metrics', action='store_true', default=True, required=False, help='Compute and score the learned value and set (needs ground truth)')
     p.add_argument('--temporal_loss', action='store_true', default=False, required=False, help='Compute the loss over time chunks (slower)')
@@ -76,12 +75,13 @@ if __name__ == '__main__':
     p.add_argument('--mulob_loss_type', type=str, default='vanilla', choices=['vanilla', 'augment', 'deform', 'augment-deform'], help='Type of multiobjective loss')
     p.add_argument('--load_decomposed_models', action='store_true', default=False, required=False, help='Will load models corresponding to the decomposed game values.')
     p.add_argument('--load_decomposed_model_name_1', type=str, default='./runs/mulob/ConveyorND/Conveyor2D/reach_only_2D', help='Decomposed value 1 model name')
-    p.add_argument('--load_decomposed_model_name_2', type=str, default='./runs/mulob/ConveyorND/Conveyor2D/avoid_only_2D_ball', help='Decomposed value 2 model name')
+    p.add_argument('--load_decomposed_model_name_2', type=str, default='./runs/mulob/ConveyorND/Conveyor2D/avoid_only_2D_fast2', help='Decomposed value 2 model name')
     p.add_argument('--super_pretrain', action='store_true', default=False, required=False, help='Pretrain with supervision losses')
     p.add_argument('--super_pretrain_iters', type=int, default=10000, required=False, help='Number of pretrain iterations with supervision losses')
     p.add_argument('--solve_grad', action='store_true', default=False, required=False, help='Compute gradients (forced true if gradient supervision), slower')
     p.add_argument('--grad_super', action='store_true', default=False, required=False, help='Supervision of not only value but gradient')
     p.add_argument('--lam_slice_super', action='store_true', default=False, required=False, help='Limits supervision losses to data slices with fixed lambda values.')
+    p.add_argument('--LS_w_time_curr', action='store_true', default=False, required=False, help='Do supervision with a temporal curriculum')
     p.add_argument('--baseline', action='store_true', default=False, required=False, help='Baseline DeepReach method (no Hopf)')
 
     ## Multi-Objective Loss Weights
@@ -209,10 +209,13 @@ if __name__ == '__main__':
     #     opt = loaded_opt
         
     if opt.debug_params:
-        opt.pretrain_iters = 2
-        opt.super_pretrain_iters = 0
-        opt.num_epochs = 20
-        opt.epochs_til_ckpt = 10
+        opt.pretrain_iters = 500
+        # opt.super_pretrain_iters = 2
+        # opt.num_epochs = 20
+        # opt.epochs_til_ckpt = 10
+        # opt.super_pretrain_iters = 2000
+        opt.num_epochs = 10000
+        opt.epochs_til_ckpt = 500
         opt.use_bank = False
 
     if opt.capacity_test:
@@ -334,46 +337,37 @@ if __name__ == '__main__':
         loaded_dynamics_inst = None
 
     ## Load Learned Models Corresponding to Decomposed Values
+    loaded_model_1, loaded_model_2 = None, None
+    loaded_dynamics_inst_1, loaded_dynamics_inst_2 = None, None
+
     if orig_opt.load_decomposed_models:
-        load_dir_1, load_dir_2 = opt.load_decomposed_model_name_1, opt.load_decomposed_model_name_2 
-        with open(os.path.join(load_dir_1, 'orig_opt.pickle'), 'rb') as opt_file:
-            loaded_opt_1 = pickle.load(opt_file)
-        with open(os.path.join(load_dir_2, 'orig_opt.pickle'), 'rb') as opt_file:
-            loaded_opt_2 = pickle.load(opt_file)
 
-        if not orig_opt.dynamics_class.endswith("lambda"):
-            loaded_model_1 = modules.SingleBVPNet(in_features=dynamics_inst.input_dim, out_features=1, type=loaded_opt_1.model, mode=loaded_opt_1.model_mode,
-                                        final_layer_factor=1., hidden_features=loaded_opt_1.num_nl, num_hidden_layers=loaded_opt_1.num_hl)
-            loaded_model_2 = modules.SingleBVPNet(in_features=dynamics_inst.input_dim, out_features=1, type=loaded_opt_2.model, mode=loaded_opt_2.model_mode,
-                                        final_layer_factor=1., hidden_features=loaded_opt_2.num_nl, num_hidden_layers=loaded_opt_2.num_hl)
-        else:
-            loaded_model_1 = modules.SingleBVPNet(in_features=dynamics_inst.input_dim-1, out_features=1, type=loaded_opt_1.model, mode=loaded_opt_1.model_mode,
-                                        final_layer_factor=1., hidden_features=loaded_opt_1.num_nl, num_hidden_layers=loaded_opt_1.num_hl)
-            loaded_model_2 = modules.SingleBVPNet(in_features=dynamics_inst.input_dim-1, out_features=1, type=loaded_opt_2.model, mode=loaded_opt_2.model_mode,
-                                        final_layer_factor=1., hidden_features=loaded_opt_2.num_nl, num_hidden_layers=loaded_opt_2.num_hl)
-        loaded_model_1.cuda()
-        loaded_model_2.cuda()
-        
-        model_path_1 = os.path.join(load_dir_1, 'training', 'checkpoints', 'model_final.pth')
-        model_path_2 = os.path.join(load_dir_2, 'training', 'checkpoints', 'model_final.pth')
-        loaded_model_1.load_state_dict(torch.load(model_path_1)['model']) # FIXME, key only needed for chkpts
-        loaded_model_2.load_state_dict(torch.load(model_path_2)['model']) # FIXME, key only needed for chkpts
-        loaded_model_1.eval()
-        loaded_model_2.eval()
+        def load_model(load_decomposed_model_name_, dynamics_inst_, dynamics_class_):
 
-        ## Loading non-lambda dynamics class of decomposed (might have diff DR model/params)
-        dynamics_class_name = orig_opt.dynamics_class if not orig_opt.dynamics_class.endswith("lambda") else orig_opt.dynamics_class.split("lambda")[0]
-        loaded_dynamics_class = getattr(dynamics, dynamics_class_name) # non-lambda version of dynamics
+            dynamics_class_name_ = dynamics_class_ if not dynamics_class_.endswith("lambda") else dynamics_class_.split("lambda")[0]
+            with open(os.path.join(load_decomposed_model_name_, 'orig_opt.pickle'), 'rb') as opt_file_:
+                loaded_opt_ = pickle.load(opt_file_)
+            
+            if not dynamics_class_.endswith("lambda"):
+                loaded_model_ = modules.SingleBVPNet(in_features=dynamics_inst_.input_dim, out_features=1, type=loaded_opt_.model, mode=loaded_opt_.model_mode,
+                                        final_layer_factor=1., hidden_features=loaded_opt_.num_nl, num_hidden_layers=loaded_opt_.num_hl)
+            else:
+                loaded_model_ = modules.SingleBVPNet(in_features=dynamics_inst_.input_dim-1, out_features=1, type=loaded_opt_.model, mode=loaded_opt_.model_mode,
+                                        final_layer_factor=1., hidden_features=loaded_opt_.num_nl, num_hidden_layers=loaded_opt_.num_hl)
 
-        loaded_dynamics_inst_1 = loaded_dynamics_class(**{argname: getattr(loaded_opt_1, argname) for argname in inspect.signature(loaded_dynamics_class).parameters.keys() if argname != 'self'})
-        loaded_dynamics_inst_1.deepreach_model=loaded_opt_1.deepreach_model
+            loaded_model_.cuda()        
+            model_path_ = os.path.join(load_decomposed_model_name_, 'training', 'checkpoints', 'model_final.pth')
+            loaded_model_.load_state_dict(torch.load(model_path_, weights_only=True)['model'])
+            loaded_model_.eval()
 
-        loaded_dynamics_inst_2 = loaded_dynamics_class(**{argname: getattr(loaded_opt_2, argname) for argname in inspect.signature(loaded_dynamics_class).parameters.keys() if argname != 'self'})
-        loaded_dynamics_inst_2.deepreach_model=loaded_opt_2.deepreach_model
-        
-    else:
-        loaded_model_1, loaded_model_2 = None, None
-        loaded_dynamics_inst_1, loaded_dynamics_inst_2 = None, None
+            loaded_dynamics_class_ = getattr(dynamics, dynamics_class_name_) # non-lambda version of dynamics
+            loaded_dynamics_inst_ = loaded_dynamics_class_(**{argname: getattr(loaded_opt_, argname) for argname in inspect.signature(loaded_dynamics_class_).parameters.keys() if argname != 'self'})
+
+            return loaded_model_, loaded_dynamics_inst_
+
+        loaded_model_1, loaded_dynamics_inst_1 = load_model(opt.load_decomposed_model_name_1, dynamics_inst, orig_opt.dynamics_class)
+
+        loaded_model_2, loaded_dynamics_inst_2 = load_model(opt.load_decomposed_model_name_2, dynamics_inst, orig_opt.dynamics_class)
 
     dataset = dataio.ReachabilityDataset(
         dynamics=dynamics_inst, numpoints=orig_opt.numpoints, 

utils/dataio.py
@@ -117,7 +117,7 @@ class ReachabilityDataset(Dataset):
         self.loaded_dynamics_1 = loaded_dynamics_1
         self.loaded_dynamics_2 = loaded_dynamics_2
         if loaded_model_1: self.loaded_model_1 = loaded_model_1.cuda()
-        if loaded_model_2: self.loaded_model_1 = loaded_model_2.cuda()
+        if loaded_model_2: self.loaded_model_2 = loaded_model_2.cuda()
         self.load_decomposed_models = loaded_model_1 is not None and loaded_model_2 is not None
 
         # Lambda Variation Options
@@ -126,7 +126,7 @@ class ReachabilityDataset(Dataset):
         self.lambda_int_2 = 0.2
         self.lambda_int_3 = 0.5
         self.zerolambda_LS = lambda_var and zerolambda_LS
-        self.LS_w_time_curr = LS_w_time_curr and use_hopf
+        self.LS_w_time_curr = LS_w_time_curr
 
         # Benchmark test only
         self.make_benchmark_gts = make_benchmark_gts
@@ -198,7 +198,7 @@ class ReachabilityDataset(Dataset):
 
         else:
             if self.super_pretrain or self.no_curriculum or (self.super_pretrained and not self.LS_w_time_curr):
-                times = self.tMin + torch.zeros(self.numpoints, 1).uniform_(0, (self.tMax-self.tMin)) # during hopf pt, sample across all time?
+                times = self.tMin + torch.zeros(self.numpoints, 1).uniform_(0, (self.tMax-self.tMin)) # during super pt, sample across all time?
             else:
                 times = self.tMin + torch.zeros(self.numpoints, 1).uniform_(0, (self.tMax-self.tMin) * (self.counter/self.counter_end))
             times[-self.num_src_samples:, 0] = self.tMin # force include initial time samples
@@ -272,7 +272,7 @@ class ReachabilityDataset(Dataset):
         
         boundary_values = self.dynamics.boundary_fn(states_io, times_io)
 
-        ## Reach-Avoif Data
+        ## Reach-Avoid Data
         if self.dynamics.loss_type == 'brat_hjivi':
             reach_values = self.dynamics.reach_fn(states_io, times_io)
             avoid_values = self.dynamics.avoid_fn(states_io, times_io)
@@ -288,7 +288,7 @@ class ReachabilityDataset(Dataset):
                 bc_values_1 = self.dynamics.reach_fn_1(states_io, times_io)
                 bc_values_2 = self.dynamics.reach_fn_2(states_io, times_io)
 
-            if not self.dataset.load_decomposed_models:
+            if not self.load_decomposed_models:
                 if not self.lambda_var:
                     if self.solve_grad:
                         gt_decomposed_values_1, gt_decomposed_grads_1 = self.V_DP_1(model_coords.t())
@@ -306,12 +306,16 @@ class ReachabilityDataset(Dataset):
                         gt_decomposed_values_1 = self.V_DP_1(model_coords[..., :-1].t())
                         gt_decomposed_values_2 = self.V_DP_2(model_coords[..., :-1].t())
                         gt_decomposed_grads_1, gt_decomposed_grads_2 = torch.empty(0), torch.empty(0)
+            else:
+                gt_decomposed_values_1, gt_decomposed_values_2, gt_decomposed_grads_1, gt_decomposed_grads_2 = torch.empty(0), torch.empty(0), torch.empty(0), torch.empty(0)
                     
                 # FIXME: for BRAT decomp, need V_DP_2 == avoid_fn (NOT avoid_value)
 
             if self.lam_slice_super and self.lambda_var:
-                model_coords_poslam = torch.cat((model_coords[..., :-1], self.dynamics.lambda_target_hi + 0*model_coords[..., -2:-1]), -1) 
-                model_coords_neglam = torch.cat((model_coords[..., :-1], self.dynamics.lambda_target_lo + 0*model_coords[..., -2:-1]), -1) 
+                norm_lambda_target_hi = self.dynamics.lambda_target_hi / self.dynamics.state_var[-1]
+                norm_lambda_target_lo = self.dynamics.lambda_target_lo / self.dynamics.state_var[-1]
+                model_coords_poslam = torch.cat((model_coords[..., :-1], norm_lambda_target_hi + 0*model_coords[..., -2:-1]), -1) 
+                model_coords_neglam = torch.cat((model_coords[..., :-1], norm_lambda_target_lo + 0*model_coords[..., -2:-1]), -1) 
             else:
                 model_coords_poslam, model_coords_neglam = model_coords, model_coords
 
@@ -341,7 +345,7 @@ class ReachabilityDataset(Dataset):
         if self.super_pretrain and self.super_pretrain_counter == self.super_pretrain_iters:
             self.super_pretrain = False
             self.super_pretrained = True
-            print("\n\n ---------------- FINISHED SUPERVISOR PRETRAINING ------------------- \n")
+            print("\n\n ---------------- FINISHED SUPERVISION PRETRAINING ------------------- \n")
 
 
         if self.dynamics.loss_type == 'brt_hjivi':

utils/losses.py
@@ -198,7 +198,7 @@ def init_mulob_hjivi_loss(experiment, minWith, dirichlet_loss_divisor, mulob_typ
                           dss_value_loss_1_divisor=10., dss_value_loss_2_divisor=10.,
                           dss_grad_loss_1_divisor=10., dss_grad_loss_2_divisor=10.,
                           lbss_value_loss_divisor=1., lbss_grad_loss_divisor=100.,
-                          grad_super=False):
+                          grad_super=False, grad_super_time=False):
     
     grad_super = grad_super and experiment.dataset.solve_grad # must solve to use
 
@@ -222,8 +222,12 @@ def init_mulob_hjivi_loss(experiment, minWith, dirichlet_loss_divisor, mulob_typ
                 decomposed_value_2_loss = values_neglam - decomposed_value_2
 
                 if grad_super:
-                    decomposed_grad_1_loss = grad_poslam[...,:-1] - decomposed_grad_1
-                    decomposed_grad_2_loss = grad_neglam[...,:-1] - decomposed_grad_2
+                    if grad_super_time:
+                        decomposed_grad_1_loss = grad_poslam[...,:-1] - decomposed_grad_1
+                        decomposed_grad_2_loss = grad_neglam[...,:-1] - decomposed_grad_2
+                    else:
+                        decomposed_grad_1_loss = grad_poslam[...,1:-1] - decomposed_grad_1[..., 1:]
+                        decomposed_grad_2_loss = grad_neglam[...,1:-1] - decomposed_grad_2[..., 1:]
 
                 if experiment.dataset.lam_slice_super:
                     dss_value_1_weight = 1 / dss_value_loss_1_divisor
@@ -250,12 +254,17 @@ def init_mulob_hjivi_loss(experiment, minWith, dirichlet_loss_divisor, mulob_typ
                 loss_dict['lbss_value_loss'] = lbss_value_loss / lbss_value_loss_divisor
 
                 if grad_super:
-
-                    if not experiment.dataset.lambda_var:
-                        lbss_grad_loss = grad - torch.cat((decomposed_grad_1, decomposed_grad_2), -1)[..., torch.argmax(decomposed_value_1, decomposed_value_2)] # debug
+                    if not experiment.dataset.lambda_var: #FIXME need to debug surely
+                        if grad_super_time:
+                            lbss_grad_loss = grad - torch.cat((decomposed_grad_1, decomposed_grad_2), -1)[..., torch.argmax(decomposed_value_1, decomposed_value_2)] # debug
+                        else:
+                            lbss_grad_loss = grad[..., 1:] - torch.cat((decomposed_grad_1, decomposed_grad_2), -1)[..., torch.argmax(decomposed_value_1, decomposed_value_2)][..., 1:] # debug
                     else:
-                        lbss_grad_loss = grad[...,:-1] - torch.cat((decomposed_grad_1, decomposed_grad_2), -1)[..., torch.argmax(decomposed_value_1, decomposed_value_2)] # debug
-
+                        if grad_super_time:
+                            lbss_grad_loss = grad[...,:-1] - torch.cat((decomposed_grad_1, decomposed_grad_2), -1)[..., torch.argmax(decomposed_value_1, decomposed_value_2)] # debug
+                        else:
+                            lbss_grad_loss = grad[...,1:-1] - torch.cat((decomposed_grad_1, decomposed_grad_2), -1)[..., torch.argmax(decomposed_value_1, decomposed_value_2)][..., 1:] # debug
+                            
                     loss_dict['lbss_grad_loss'] = lbss_grad_loss / lbss_grad_loss_divisor
 
                 # TODO?: for deformation, move dynamic weight multiplication in here for organization (scheduler can remain out)
@@ -270,7 +279,7 @@ def init_mulob_hjivi_loss(experiment, minWith, dirichlet_loss_divisor, mulob_typ
             # Lambda shift (non-lambda) decomposed values
             if experiment.dataset.lambda_var:
                 decomposed_value_1 = decomposed_value_1 - torch.nn.functional.relu(-state[..., -1])
-                decomposed_value_2 = decomposed_value_2 - torch.nn.functional.relu(state[..., -1])
+                decomposed_value_2 = decomposed_value_2 + torch.nn.functional.relu(state[..., -1])
 
             ## BRT
             if minWith == 'zero':
@@ -284,8 +293,14 @@ def init_mulob_hjivi_loss(experiment, minWith, dirichlet_loss_divisor, mulob_typ
 
             elif mulob_type == 'BRAAT':
 
-                diff_constraint_hom = torch.min(torch.max(dvdt - ham, value + bc_value_2), 
-                                                torch.max(value - bc_value_1, value + decomposed_value_2))
+                # diff_constraint_hom = torch.min(torch.max(dvdt - ham, value + bc_value_2), 
+                #                                 torch.max(value - bc_value_1, value + decomposed_value_2))
+                
+                diff_constraint_hom = torch.min(torch.max(dvdt - ham, value - bc_value_2), 
+                                                torch.max(value + bc_value_1, value - decomposed_value_2))
+                
+                # diff_constraint_hom = torch.min(torch.max(dvdt - ham, value + bc_value_2), 
+                #                                 torch.max(value - bc_value_1, -(value + decomposed_value_2)))
                 
             elif mulob_type == 'BRRT':
 